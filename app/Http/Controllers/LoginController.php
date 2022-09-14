83ac6b72a4aca9b8dae58a436fdc4d88638e9b3c
Isac999 <isac.costafernandes@hotmail.com>
2022-09-14 14:10:11 -0300

    changes

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login() {
+        return view('login');
+    }
+
+    public function verificacao(Request $request) {
+        /*
+        $request->validate([
+            'email' => 'required|email|min:8|unique:logins',
+            'password' => 'required|min:6|max:25'
+        ]);
+        */
+        return 'tudo ok';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [App\Http\Controllers\LoginController::class, 'login'])
+->name('login.index');
+Route::post('/', [App\Http\Controllers\LoginController::class, 'verificacao'])
+->name('login.index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
