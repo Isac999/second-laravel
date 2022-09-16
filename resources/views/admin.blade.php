a4f87c127bd03b4c8f804259b341c31d2cc7ed5c
Isac999 <isac.costafernandes@hotmail.com>
2022-09-16 10:07:06 -0300

    views

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts_admin.base')
+
+@section('main')
+    <br> <br>
+    <div class="container mt-4 mb-4"><br>
+        <div class="text-center">
+            <h1 class="mt-1"> Welcome to the admin panel! </h1>
+            <p>Create, update, delete and read data quickly!</p> 
+        </div>
+        <div class="row justify-content-center">
+            <table class="table table-striped">
+                <thead class="text-white font-weight-bold" style="background-color: #146176">
+                    <tr>
+
+                            contenttttt
+
+                    </tr>
+                </thead>
+
+                    rows render
+
+            </table>
+        </div>
+    </div>
+    <div class="row justify-content-center mb-5">
+
+                paginação
+
+    </div>
+@endsection
---
+++ resources/views/layouts_admin/header.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
+    <div class="container">
+        <a class="navbar-brand text-white font-weight-bold"> Admin Control</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse row justify-content-around" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                <a class="nav-link text-white" id="books" href="?page=books&pagina=1">Books</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link text-white" id="customers" href="?page=customers&pagina=1">Customers</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link text-white" id="books_rentals" href="?page=books_rentals&pagina=1">Books Rentals</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link text-white" id="requests_to_suppliers" href="?page=requests_to_suppliers&pagina=1">Requests to Suppliers</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link text-white" id="suppliers" href="?page=suppliers&pagina=1">Suppliers</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link text-white" id="libraries" href="?page=libraries&pagina=1">Libraries</a>
+                </li>
+            </ul>
+        </div>
+        <a class="navbar-brand text-white font-weight-bold" href="./src/Utils/Logout.php">Logout</a>
+    </div>
+</nav>
