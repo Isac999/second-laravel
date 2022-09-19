c4840e4947d323727415e806bdce1cf820a04cfe
Isac999 <fisac0040@gmail.com>
2022-09-19 11:10:41 -0300

    changes

--- app/Http/Controllers/RequestsToSuppliersController.php
+++ app/Http/Controllers/RequestsToSuppliersController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\RequestToSuppliers;
+use App\Models\RequestsToSuppliers;
 use Illuminate\Support\Facades\Schema;
 
 class RequestsToSuppliersController extends Controller
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,16 @@ Route::prefix('/app')->group(function() {
     ->name('app.index');
     Route::get('/logout', [App\Http\Controllers\LogoutController::class, 'index'])
     ->name('app.logout');
+    Route::get('/admin/books', [App\Http\Controllers\BooksController::class, 'index'])
+    ->name('app.index');
+    Route::get('/admin/customers', [App\Http\Controllers\CustomersController::class, 'index'])
+    ->name('app.index');
+    Route::get('/admin/books_rentals', [App\Http\Controllers\BooksRentalsController::class, 'index'])
+    ->name('app.index');
+    Route::get('/admin/libraries', [App\Http\Controllers\LibrariesController::class, 'index'])
+    ->name('app.index');
+    Route::get('/admin/requests_to_suppliers', [App\Http\Controllers\RequestsToSuppliersController::class, 'index'])
+    ->name('app.index');
+    Route::get('/admin/suppliers', [App\Http\Controllers\SuppliersController::class, 'index'])
+    ->name('app.index');
 });
