e0d08fc7fa7fff28525ac6df60225aedd899c20b
Isac999 <fisac0040@gmail.com>
2022-09-20 13:42:21 -0300

    changes

--- public/js/index.js
+++ public/js/index.js
@@ -2,19 +2,24 @@ const list_btn = document.querySelectorAll('#edit');
 
 list_btn.forEach((element) => {
     element.addEventListener('click', (btn) => {
+
         let row = (btn.target.parentElement).parentElement;
         for (item of row.children) {
+
             if (item.id != 'package') {
                 let innerContent = item.innerText;
                 let input = document.createElement('input');
                 input.setAttribute('value', innerContent);
+                
                 item.innerText = '';
                 item.appendChild(input);
-            } else {
+            } 
+            else {
                 let btn = document.createElement('button');
                 btn.setAttribute('class', 'btn btn-warning');
                 btn.innerText = 'Change';
-                (item.children)[0].replaceWith(btn);
+            
+                let newBtn = (item.children)[0].replaceWith(btn);
             }
         }
     })
