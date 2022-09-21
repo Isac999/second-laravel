ed58f3cdce4e14d6d162dd9f9dde3c4cfc9969f4
Isac999 <fisac0040@gmail.com>
2022-09-21 10:16:46 -0300

    changes

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,18 @@
+const list_btn = document.querySelectorAll('#delete');
+
+list_btn.forEach((element) => {
+    element.addEventListener('click', (btn) => {
+        const value = window.confirm('Tem certeza que deseja remover este item da lista?');
+        if (value) {
+            const tr = btn.path[2];
+            const id = tr.firstElementChild.innerText;
+
+            tr.remove();
+            sendIdRemove(id);
+        }
+    })
+})
+
 function alterBtn(btn) {
     const row = (btn.parentElement).parentElement;
         
@@ -48,62 +63,3 @@ function sendDataEdit(list) {
 function sendIdRemove(id) {
     console.log(id);
 }
-
-const list_btn = document.querySelectorAll('#delete');
-list_btn.forEach((element) => {
-    element.addEventListener('click', (btn) => {
-        const value = window.confirm('Tem certeza que deseja remover este item da lista?');
-        if (value) {
-            const tr = btn.path[2];
-            const id = tr.firstElementChild.innerText;
-
-            tr.remove();
-            sendIdRemove(id);
-        }
-    })
-})
-/*
-alterEdit();
-
-function alterEdit() {
-    
-
-    list_btn.forEach((element) => {
-        console.log(element);
-        element?.addEventListener('click', (btn) => {
-            const row = (btn.target.parentElement).parentElement;
-            
-            for (item of Array.from(row?.children)) {
-    
-                if (item.id != 'package') {
-                    let innerContent = item.innerText;
-    
-                    let input = document.createElement('input');
-                    input.setAttribute('value', innerContent);
-    
-                    item.innerText = '';
-                    item.appendChild(input);
-                } 
-                else {
-                    let btn = document.createElement('button');
-                    btn.setAttribute('class', 'btn btn-warning');
-                    btn.innerText = 'Change';
-                
-                    (item.children)[0].replaceWith(btn);
-                    item.children[0].addEventListener('click', (item) => {
-                        sendDataEdit(row);
-    
-                        let btn = document.createElement('button');
-                        btn.setAttribute('class', 'btn btn-info');
-                        btn.setAttribute('id', 'edit');
-                        btn.innerText = 'Edit';
-                        //console.log(item);
-                        item.path[0].replaceWith(btn);
-                        //alterEdit();
-                    })
-                }
-            }
-        })
-    })
-}
-*/
