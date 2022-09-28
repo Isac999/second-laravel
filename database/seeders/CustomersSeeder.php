7fb62f6afe25b9fcebd6d9c2a6551c303c4424e8
Isac999 <fisac0040@gmail.com>
2022-09-28 10:06:55 -0300

    changes

--- database/seeders/CustomersSeeder.php
+++ database/seeders/CustomersSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Customers;
 
 class CustomersSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class CustomersSeeder extends Seeder
      */
     public function run()
     {
-        //
+        App\Models\Customers::factory()->count(100)->create();
     }
 }
